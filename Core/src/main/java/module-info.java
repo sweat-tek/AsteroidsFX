7efeee3f49cbb3a53c0e5ae29a30694c018b7cd1
corfixen <corfixen@gmail.com>
2024-02-06 19:51:33 +0100

    Debugger commented out.

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -5,7 +5,6 @@ module Core {
     opens dk.sdu.mmmi.cbse.main to javafx.graphics;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
-
 }
 
 
