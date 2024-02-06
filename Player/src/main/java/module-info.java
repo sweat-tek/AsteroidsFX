0312f1d890eb0f6afc1e2553f743196b1927723c
corfixen <corfixen@gmail.com>
2024-02-06 19:56:41 +0100

    Clean up

--- Player/src/main/java/module-info.java
+++ Player/src/main/java/module-info.java
@@ -4,10 +4,8 @@ import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
 module Player {
     requires Common;
-    requires CommonBullet;
-    requires javafx.graphics;    
-    //requires CommonBullet;
-    //uses dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
+    requires CommonBullet;   
+    uses dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
     provides IGamePluginService with dk.sdu.mmmi.cbse.playersystem.PlayerPlugin;
     provides IEntityProcessingService with dk.sdu.mmmi.cbse.playersystem.PlayerControlSystem;
     
