7080795d99a88e4733f3fd2220eb616f8c13bbf3
looseMole <62012079+looseMole@users.noreply.github.com>
2024-02-07 11:57:39 +0100

    Comments + Movement added to BulletControlSystem

--- Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletControlSystem.java
+++ Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletControlSystem.java
@@ -10,15 +10,18 @@ import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 public class BulletControlSystem implements IEntityProcessingService, BulletSPI {
 
     @Override
-    public void process(GameData gameData, World world) {
+    public void process(GameData gameData, World world) { // The behaviour of the bullet?
 
-        for (Entity bullet : world.getEntities(Bullet.class)) {
-
+        for (Entity bullet : world.getEntities(Bullet.class)) { // Movement of the bullet, stolen from PlayerControlSystem's reaction to a press of the "up" key.
+            double changeX = Math.cos(Math.toRadians(bullet.getRotation()));
+            double changeY = Math.sin(Math.toRadians(bullet.getRotation()));
+            bullet.setX(bullet.getX() + changeX);
+            bullet.setY(bullet.getY() + changeY);
         }
     }
 
     @Override
-    public Entity createBullet(Entity shooter, GameData gameData) {
+    public Entity createBullet(Entity shooter, GameData gameData) { // Takes the entity which fires the bullet, and gets the gameData injected.
 
         return null;
     }
