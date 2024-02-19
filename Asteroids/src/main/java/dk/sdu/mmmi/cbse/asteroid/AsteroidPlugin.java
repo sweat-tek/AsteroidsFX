d05b6481c06c83ff7bdf82e0683daf678071d2bd
corfixen <corfixen@gmail.com>
2024-02-19 14:56:55 +0100

    Simple Asteroids system

--- Asteroids/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidPlugin.java
+++ Asteroids/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidPlugin.java
@@ -5,6 +5,8 @@ import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import java.util.Random;
+
 
 /**
  *
@@ -15,8 +17,10 @@ public class AsteroidPlugin implements IGamePluginService {
     @Override
     public void start(GameData gameData, World world) {
         // Add entities to the world
-        Entity asteroid = createAsteroid(gameData);
-        world.addEntity(asteroid);
+        for (int i = 0; i < 10; i++) {
+            Entity asteroid = createAsteroid(gameData);
+            world.addEntity(asteroid);
+        }        
     }
 
     @Override
@@ -29,7 +33,12 @@ public class AsteroidPlugin implements IGamePluginService {
 
     private Entity createAsteroid(GameData gameData) {
         Entity asteroid = new Asteroid();
-
+        Random rnd = new Random();
+        int size = rnd.nextInt(10) + 5 ;
+        asteroid.setPolygonCoordinates(size,-size,-size,-size,-size,size, size, size);        
+        asteroid.setX(0);
+        asteroid.setY(0);
+        asteroid.setRotation(rnd.nextInt(90));
         return asteroid;
     }
 }
--- Asteroids/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidProcessor.java
+++ Asteroids/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidProcessor.java
@@ -15,6 +15,28 @@ public class AsteroidProcessor implements IEntityProcessingService {
     public void process(GameData gameData, World world) {
 
         for (Entity asteroid : world.getEntities(Asteroid.class)) {
+        double changeX = Math.cos(Math.toRadians(asteroid.getRotation()));
+        double changeY = Math.sin(Math.toRadians(asteroid.getRotation()));
+
+        asteroid.setX(asteroid.getX() + changeX * 0.5);
+        asteroid.setY(asteroid.getY() + changeY * 0.5);
+
+        if (asteroid.getX() < 0) {
+            asteroid.setX(asteroid.getX() - gameData.getDisplayWidth());
+        }
+
+        if (asteroid.getX() > gameData.getDisplayWidth()) {
+            asteroid.setX(asteroid.getX() % gameData.getDisplayWidth());
+        }
+
+        if (asteroid.getY() < 0) {
+            asteroid.setY(asteroid.getY() - gameData.getDisplayHeight());
+        }
+
+        if (asteroid.getY() > gameData.getDisplayHeight()) {
+            asteroid.setY(asteroid.getY() % gameData.getDisplayHeight());
+        }
+            
         }
 
     }
