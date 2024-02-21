3fb614c4c3ec9f872244355284f23019b61a1f2f
gokm8 <107099606+gokm8@users.noreply.github.com>
2024-02-21 12:09:58 +0100

    adding files

---
+++ Asteroids/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module Asteoriod {
+    requires Common;
+    requires CommonAsteorid;
+
+
+}
---
+++ CommonAsteorid/src/main/java/dk/sdu/mmmi/asteorids/Asteorid.java
@@ -0,0 +1,6 @@
+package dk.sdu.mmmi.asteorids;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+
+public class Asteorid extends Entity {
+}
