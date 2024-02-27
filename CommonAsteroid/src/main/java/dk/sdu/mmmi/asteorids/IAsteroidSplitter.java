e2da8bb0f60c647b310e5a8975aeebf9855c5b41
gokm8 <107099606+gokm8@users.noreply.github.com>
2024-02-27 11:38:00 +0100

    deleted IAsteoridSplitter.java

--- CommonAsteroid/src/main/java/dk/sdu/mmmi/asteorids/IAsteroidSplitter.java
+++
@@ -1,8 +0,0 @@
-package dk.sdu.mmmi.asteorids;
-
-import dk.sdu.mmmi.cbse.common.data.Entity;
-import dk.sdu.mmmi.cbse.common.data.World;
-
-public interface IAsteroidSplitter {
-    void createSplitAsteroid(Entity e, World w);
-}
