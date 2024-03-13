70d0feb9ac0f977c8de4d6b9eb3c45f3ad6c409c
bruger <Wesam.ba12@gmail.com>
2024-03-13 11:28:59 +0100

    Bullet system implemted Java info moudel

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module Core {
     requires Common;
-    requires CommonBullet;    
-    requires javafx.graphics;    
-    opens dk.sdu.mmmi.cbse.main to javafx.graphics;
+    requires CommonBullet;
+    requires javafx.graphics;
+    requires spring.context;
+    requires spring.core;
+    requires spring.beans;
+    exports dk.sdu.mmmi.cbse.main;
+    opens dk.sdu.mmmi.cbse.main to javafx.graphics, spring.core;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
--- Player/src/main/java/module-info.java
+++ Player/src/main/java/module-info.java
@@ -1,12 +1,15 @@
-
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
-module Player {
+
+module Player{
+
     requires Common;
-    requires CommonBullet;   
+    requires CommonBullet;
     uses dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
     provides IGamePluginService with dk.sdu.mmmi.cbse.playersystem.PlayerPlugin;
     provides IEntityProcessingService with dk.sdu.mmmi.cbse.playersystem.PlayerControlSystem;
-    
+
+
 }
+
