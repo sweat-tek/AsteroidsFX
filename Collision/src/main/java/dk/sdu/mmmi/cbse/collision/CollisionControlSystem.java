bddc9c441d4e46b00c86321c9f0076cc8537659e
gokm8 <107099606+gokm8@users.noreply.github.com>
2024-04-03 10:34:26 +0200

    Update CollisionControlSystem.java

--- Collision/src/main/java/dk/sdu/mmmi/cbse/collision/CollisionControlSystem.java
+++ Collision/src/main/java/dk/sdu/mmmi/cbse/collision/CollisionControlSystem.java
@@ -15,7 +15,7 @@ public class CollisionControlSystem implements IPostEntityProcessingService {
                         Math.pow(entity1.getX() - entity2.getX(), 2)
                         + Math.pow((entity1.getY() - entity2.getY()), 2));
 
-                if (distance < entity1.getSize() && distance < entity2.getSize()) {
+                if (distance < entity1.getSize() || distance < entity2.getSize()) {
                     entity1.setDead(true);
                     entity2.setDead(true);
                     entity1.setHealth(entity1.getHealth()-entity2.getSize());
